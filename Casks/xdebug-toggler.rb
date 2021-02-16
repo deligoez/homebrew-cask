15b5a64cb81a8272940e5b7984fa0846fdb76a45
Yunus Emre Deligöz <3030815+deligoez@users.noreply.github.com>
2021-02-16 21:20:20 +0300

    Update xdebug-toggler.rb

--- Casks/xdebug-toggler.rb
+++ Casks/xdebug-toggler.rb
@@ -1,11 +1,11 @@
-cask 'xdebug-toggler' do
-   version '1.1.0'
-   sha256 '025d25ab467ac76de911bd226d0836e57d3bae32f2e611850527879d549b0aad'
+cask "xdebug-toggler" do
+   version "1.2.0"
+   sha256 "7c21a401a49da467515b1e693f9551378a1fba717fba423cfb720c9ca3d37ed6"
 
    url "https://github.com/deligoez/xDebug-Toggler/releases/download/#{version}/xDebugToggler.dmg"
-   appcast 'https://github.com/deligoez/xDebug-Toggler/releases.atom'
-   name 'xDebugToggler'
-   homepage 'https://github.com/deligoez/xDebug-Toggler'
+   appcast "https://github.com/deligoez/xDebug-Toggler/releases.atom"
+   name "xDebugToggler"
+   homepage "https://github.com/deligoez/xDebug-Toggler"
 
-   app 'xDebugToggler.app'
+   app "xDebugToggler.app
  end
